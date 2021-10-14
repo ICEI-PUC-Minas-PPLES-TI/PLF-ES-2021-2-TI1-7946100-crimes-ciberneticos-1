801317f53f694000595a797c202a25134393fc8a
m4theuzzz <mat.vsantos@outlook.com>
2021-10-14 19:18:14 -0300

    iniciando lógica de busca de perguntas

---
+++ Codigo/app/data/Questions.js
@@ -0,0 +1,12 @@
+const QUESTIONS = [
+    {
+        type: "",
+        subject: "",
+        challenge: 0,
+        priority: 0,
+        completed: false,
+        question: "",
+        answers: ["", "", "", ""],
+        correctAnswersIndex: []
+    }
+];
--- Codigo/app/public/js/Quiz.js
+++ Codigo/app/public/js/Quiz.js
@@ -1,3 +1,52 @@
-const init = () => {
+const filterByType = (questions, type) => {
+    return questions.filter((quest) => {
+        if (quest.type == type) {
+            return quest;
+        }
+    });
+};
 
-}
+const filterAlreadyDoneQuestions = (questions) => {
+    return questions.reduce((acc, cur) => {
+        cur.completed ? acc.done.push(cur) : acc.notDone.push(cur);
+        return acc;
+    }, { done: [], notDone: [] });
+};
+
+const orderByPriority = (questions) => {
+    return questions.sort(function (a, b) {
+        if (a.priority > b.priority) {
+            return 1;
+        }
+        if (a.priority < b.priority) {
+            return -1;
+        }
+
+        return 0;
+    });
+};
+
+const fillQuestionsArray = (preSelected, questions) => {
+    for (let i = 0; i < questions.length; i++) {
+        if (preSelected.length >= 10) {
+            break;
+        }
+        preSelected.push(questions[i]);
+    }
+
+    return preSelected;
+};
+
+const fetchQuestions = (type) => {
+    const quests = type ? filterByType(QUESTIONS, type) : QUESTIONS;
+    const { neverDone, alreadyDone } = filterAlreadyDoneQuestions(quests);
+    const firstChoices = orderByPriority(neverDone);
+    const seccondChoices = orderByPriority(alreadyDone);
+
+    let questions = fillQuestionsArray([], firstChoices)(questions, seccondChoices);
+    return questions;
+};
+
+const init = () => {
+    console.log(fetchQuestions());
+};
