6e597c00ae0fb3b39f45fff7df7b838680b6b6b6
¨eduardo <eduardo.esteves.1351017@sga.pucminas.br>
2021-10-26 20:09:08 -0300

    novas alterações

--- Codigo/app/public/js/firstAcces.js
+++ Codigo/app/public/js/firstAcces.js
@@ -1,11 +1,39 @@
 function firstAccess() {
-    if(JSON.parse(localStorage.getItem("firstAccess")) != 1) {
-        localStorage.setItem("firstAccess", 1);
-        bemVindo();
+    let first = JSON.parse(localStorage.getItem("firstAccess"));
+    if(document.title == "Home") {
+        if(first.home == 0) {
+            first.home = 1;
+            bemVindo();
+        }
     }
+    else if (document.title == "Quiz") {
+        if(first.quiz == 0) {
+            first.quiz = 1;
+            bemVindo();
+        }
+    }
+    else if (document.title == "Aprender") {
+        if(first.aprender == 0) {
+            first.aprender = 1;
+            bemVindo();
+        }
+    }
+    else if (document.title == "Perfil") {
+        if(first.perfil == 0) {
+            first.perfil = 1;
+            bemVindo();
+        }
+    }
+    else if (document.title == "Configurações") {
+        if(first.config == 0) {
+            first.config = 1;
+            bemVindo();
+        }
+    }
+    localStorage.setItem("firstAccess", JSON.stringify(first));
 }
 
 function bemVindo() {
     $('#myModal').modal("show");
-    
 } 
+
